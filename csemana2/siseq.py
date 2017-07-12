92c15afe25425e94bf8755492c2d25d8a1d6c82f
jufecabrera <jufecabrera@gmail.com>
2017-07-11 19:23:26 -0500

    siseq actualizado

--- csemana2/siseq.py
+++ csemana2/siseq.py
@@ -1,15 +1,17 @@
 import numpy as np
-A = np.matrix('1. 2. 3. 0.; 4. 5. 6. 0.; 7. 12. 9. 0.') 
-for i in range(0,len(A)):
-	A[i]=A[i]/A[i,i]
-	for j in range((i+1),len(A)):
-		if (j==len(A)):
-			break
-		A[j]= A[j]- A[j,i]*A[i]
-print A
+A = np.array([[1., 2., 3., 5., 1.], [2., 2., 5., 6., 0.],[11., 7., 12., 9., 0.],[32., 1., 23., 2., 5.]])
+print "la Matriz aumentada original: \n", A
+l,k = np.shape(A)
+for i in range(0,l):
+	A[i]=A[i]/A[i][i]
+	for j in range((i+1),l):
+		A[j]= A[j]- A[j][i]*A[i]
+print "la matriz aumentada reducida: \n", A
 x = []
-k = len(A)-1
-while (k>=0):
-	for i in range(k,len(A))
-	s = np.sum()
-	np.insertA[]
+o = np.ones(k)
+for i in reversed(range(0,l)):
+	A[i] = A[i]*o
+	s = np.sum(A[i][i+1:l])
+	o[i] = A[i][k-1]-s
+	x.insert(0,o[i])
+print "El vector solucion: \n", x
