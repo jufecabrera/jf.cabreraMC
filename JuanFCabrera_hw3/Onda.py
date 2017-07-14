ae5d204acd30f902cf15bbe705021b3810af6d80
jufecabrera <jufecabrera@gmail.com>
2017-07-14 09:48:08 -0500

    Onda iniciado, planetas animado y creacion de planetaas en tarea

--- JuanFCabrera_hw3/Onda.py
+++ JuanFCabrera_hw3/Onda.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+M = np.zeros((300,300))
+M[100,150]=-0.5
+dx,dy=30./len(M[0])
+t_max=60
+points = 600
+dt = float(t_max)/points
+for k in range(points):
+	M_past = M.copy()
+	for j in range(1,points-1):
+		 
+	 
--- csemana5/planetas.py
+++ csemana5/planetas.py
@@ -13,22 +13,29 @@ y1=datos[:,5]
 z1=datos[:,6]
 plt.plot(t,x0)
 plt.plot(t,x1)
-plt.show()
+#plt.show()
 plt.close()
 plt.plot(t,y0)
 plt.plot(t,y1)
-plt.show()
+#plt.show()
 plt.close()
 plt.plot(t,z0)
 plt.plot(t,z1)
-plt.show()
-plt.close()
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
-#line, = ax.plot([],[],[],'ko')
-#def animate(i):
-#	line.set_data([x0[i],x1[i]],[y0[i],y1[i]],[z0[i],z1[i]])
-#	return line,
-#ani = animation.FuncAnimation(fig, animate, np.arange(1, n_points),interval=250, blit=True)
 #plt.show()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x1,y1,z1)
+ax.plot(x0,y0,z0)
+line, = ax.plot([],[],[],'o')
+line1, = ax.plot([],[],[],'o')
+def animate(i):
+	line.set_data([x1[i]],[y1[i]])
+	line.set_3d_properties([z1[i]])
+	line1.set_data([x0[i]],[y0[i]])
+	line1.set_3d_properties([z0[i]])
+	return line,line1,
+ani = animation.FuncAnimation(fig, animate, np.arange(1, n_points),interval=25, blit=True)
+plt.show()
 
