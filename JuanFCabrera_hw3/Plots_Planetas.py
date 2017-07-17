71a8f241e64dec584ca166cfbb95f42d84a2aab4
Guest <guest-x9BAG2@compufi25.uniandes.edu.co>
2017-07-17 18:43:51 -0500

    animacion corregida

--- JuanFCabrera_hw3/Plots_Planetas.py
+++ JuanFCabrera_hw3/Plots_Planetas.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 datos = np.loadtxt('planetas.csv', delimiter=',')
+datos = datos[::1000]
 n_points=len(datos[:,0])
 l=np.size(datos[0])
 t = datos[:,0]
@@ -19,7 +20,7 @@ def animate(i):
 	for j in range(10):
 		globals()['line%s' % j].set_data([globals()['d%s' % j][i]],[globals()['d1%s' % j][i]])
 		globals()['line%s' % j].set_3d_properties([globals()['d2%s' % j][i]])
-	for j in range(10):		
-		return globals()['line%s' % j],
-ani = animation.FuncAnimation(fig, animate, np.arange(1, n_points),interval=2, blit=True)
+	lines = [globals()['line%s' % j] for j in range(10)]
+	return tuple(lines)	
+ani = animation.FuncAnimation(fig, animate, np.arange(1, n_points),interval=8, blit=False)
 plt.show()
