342c230ba40234e99035957c8c4abd59c21d8db1
jufecabrera <jufecabrera@gmail.com>
2017-07-18 21:16:43 -0500

    titulos de graficas

--- JuanFCabrera_hw3/Onda.py
+++ JuanFCabrera_hw3/Onda.py
@@ -3,34 +3,42 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 x = y =np.linspace(0,30,300)
 dx = dy = 30./300
-dt = dx*0.7
+dt = dx*np.sqrt(0.5)
 r2=(dt/dx)**2
 t_max= 60
 steps = t_max/dt
 M = np.zeros((int(steps),300,300))
+for i in range(191,209):
+	for j in range(141,159):
+		M[0,i,j]=-0.5*np.exp(-((i-200)**2+(j-150)**2)/2)
 M[:,100,0:130]=-2
 M[:,100,-130:-1]=-2
-M[0,200,150]=-0.5
 #t=1
 for i in range(1,299):
-	for j in range(1,290):
+	for j in range(1,299):
 		if (i==99 or i==100 or i==101):
 			if (j<=131 or j>=169):
 				continue
 		M[1,i,j] =r2/2*(M[0,i+1,j]+M[0,i-1,j]-4*M[0,i,j]+M[0,i,j+1]+M[0,i,j-1])+M[0,i,j]
 #todos los tiempos
 for k in range(1,int(steps)-1):
-	if (k%5==0):	
-		print k
 	for i in range(1,299):
 		for j in range(1,299):
 			if (i==99 or i==100 or i==101):
 				if (j<=131 or j>=169):
 					continue
 			M[k+1,i,j] = r2*(M[k,i+1,j]+M[k,i-1,j]-4*M[k,i,j]+M[k,i,j+1]+M[k,i,j-1])+2*M[k,i,j]-M[k-1,i,j]
-
+M = M+2
+plt.pcolormesh(x,y,M[423])
+plt.title('T = 30')
+plt.figsave('30.png')
+plt.close()
+plt.pcolormesh(x,y,M[847])
+plt.title('T = 60')
+plt.figsave('60.png')
+plt.close()
 fig, ax = plt.subplots(figsize=(7, 6))
-cax = ax.pcolormesh(x,y,M[0,:-1, :-1],vmin=-0.01, vmax=0.01,cmap='seismic')
+cax = ax.pcolormesh(x,y,M[0,:-1, :-1],vmin=1.99, vmax=2.01,cmap='seismic')
 fig.colorbar(cax)
 def animate(i):
 	cax.set_array(M[i,:-1, :-1].flatten())
@@ -38,6 +46,7 @@ def animate(i):
 ani = FuncAnimation(fig, animate, np.arange(1,int(steps)),interval=5, blit=False)
 plt.show()
 #ani.save('Onda.mp4')
+plt.close()
 
 
 
--- JuanFCabrera_hw3/Plots_Planetas.py
+++ JuanFCabrera_hw3/Plots_Planetas.py
@@ -7,15 +7,34 @@ datos = datos[::1000]
 n_points=len(datos[:,0])
 l=np.size(datos[0])
 t = datos[:,0]
+n =['Sun','Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
+c=['gold','k','y','g','r','c','m','b','lime','gray']
 for i in range(1,l):
 	ii = i-1
 	globals()['d%s' % ii] = datos[:,i]
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111, projection='3d')
+for i in range(10):
+	ax1.plot(globals()['d%s' % i],globals()['d1%s' % i],globals()['d2%s' % i], label = n[i])
+ax1.set_title('Orbitas de los Planetas')
+ax1.set_xlabel('x (AU)')
+ax1.set_ylabel('y (AU)')
+ax1.set_zlabel('z (AU)')
+ax1.legend()
+plt.tight_layout()
+plt.savefig('orbitas.png')
+plt.close()
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for i in range(10):
-	ax.plot(globals()['d%s' % i],globals()['d1%s' % i],globals()['d2%s' % i])
-	globals()['line%s' % i], = ax.plot([],[],[],'o')
+	ax.plot(globals()['d%s' % i],globals()['d1%s' % i],globals()['d2%s' % i],color=c[i], label=n[i])
+	globals()['line%s' % i], = ax.plot([],[],[],'o',color=c[i])
+ax.set_title('Orbitas de los Planetas')
+ax.set_xlabel('x (AU)')
+ax.set_ylabel('y (AU)')
+ax.set_zlabel('z (AU)')
+ax.legend()
 def animate(i):
 	for j in range(10):
 		globals()['line%s' % j].set_data([globals()['d%s' % j][i]],[globals()['d1%s' % j][i]])
@@ -24,3 +43,5 @@ def animate(i):
 	return tuple(lines)	
 ani = animation.FuncAnimation(fig, animate, np.arange(1, n_points),interval=8, blit=False)
 plt.show()
+#ani.save('Planetas.mp4')
+plt.close()
