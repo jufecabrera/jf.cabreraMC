82770a0c415c151102523dcf91d8cda7318a0770
jufecabrera <jufecabrera@gmail.com>
2017-07-26 10:04:09 -0500

    clase miercoles

--- JuanFCabrera_hw4/PuntoGeographicPoint.c
+++ JuanFCabrera_hw4/PuntoGeographicPoint.c
@@ -2,16 +2,19 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-#define L 10000
+#define iter 10000
 
 int lx = 744;
 int ly = 500;
 int ind(int y, int x);
 double randnum(void);
+double get_r(int* M,int x, int y);
+void imprim(int* M);
 void main(void)
 {
 	int i=0, j=0;
 	int *M = malloc((lx*ly)*sizeof(int));
+	double *L = malloc((iter*3)*sizeof(double));
 	FILE *file;
 	file = fopen("map_data.txt", "r");
 	int len = 2000;
@@ -36,12 +39,26 @@ void main(void)
 int ind(int y, int x)
 {
 	int a = y*lx+x;
-	printf("%e\n",randnum());
 	return a;
 }
 double randnum(void)
 {
-    return (double) rand()/RAND_MAX;
+	double ran =    2*((double) rand()/RAND_MAX)-1;
+	return ran;
+}
+void first(double* L, int* M)
+{
+	int agua = 1;
+	while(agua)
+	{
+		
+	}
+}
+void algor(int* M)
+{
+	int xa;
+	int ya;
+	
 }
 double get_r(int* M,int x, int y)
 {
@@ -59,20 +76,43 @@ double get_r(int* M,int x, int y)
 		{
 			xi = x-150+j;
 			yi = y-150+i;
-			if((xi<0)||(xi>743)||(yi<0)||(yi>499))
+			xi = xi%lx;
+			yi = yi%ly;
+			if(xi<0)
 			{
-				continue;
+				xi = xl+xi;
+			}
+			if(yi<0)
+			{
+				yi = yl+yi;
 			}		
-			p = M[ind(xi,yi)];
+			p = M[ind(yi,xi)];
 			if(p==0)
 			{
 				continue;
 			}	
-			r = pow()
+			r = sqrt(pow(xi-x,2)+pow(yi-y,2));
+			if(r<min)
+			{
+				min = r;
+			}		
 		}
 	}
+	return min;
 }
-
-
+void imprim(int* M)
+{
+	int i,j;
+	FILE *f = fopen("m.txt","w");
+	for(j=0;j<ly;j++)
+	{
+		for(i=0;i<lx;i++)
+		{
+			fprintf(f, "%d ",M[ind(j,i)]);
+		}
+		fprintf(f, "\n");
+	}
+	fclose(f);
+}
 
 
